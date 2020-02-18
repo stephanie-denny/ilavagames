a553ed1a5b1d373837834ed8ed3fa2d90aa2df7f
Stephanie Denny <stephanie_denny@volusion.com>
2020-02-17 19:31:21 -0600

    update marketing section

--- src/components/MarketingSection.js
+++ src/components/MarketingSection.js
@@ -18,21 +18,21 @@ const MarketingSection = ({ marketing }) => {
       <div className="container p-5">
         <Row>
           <Col className="col-12 col-md-6 text-center text-md-left">
-            <h2>{marketing.contributorTitle}</h2>
+            <h3>{marketing.contributorTitle}</h3>
             <p>{marketing.contributorText}</p>
             <Link to="/contact" className="Button secondary">
               Learn More
             </Link>
           </Col>
           <Col className="col-12 col-md-6 text-center text-md-left mt-5 mt-md-0 newsletter">
-            <h2>
+            <h3>
               {marketing.newsletterTitle}
               <FA
                 name="envelope"
                 style={{ color: 'var(--secondary)' }}
                 className="ml-4"
               />
-            </h2>
+            </h3>
             <p>{marketing.newsletterText}</p>
             <SimpleForm />
           </Col>
