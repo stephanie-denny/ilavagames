cb76242ba95b3ba28ce587dc35563a54f5b10fd2
Stephanie Denny <stephanie_denny@volusion.com>
2020-02-23 17:59:33 -0600

    Add 404 video component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,40 +1,43 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { Link, StaticQuery, graphql } from 'gatsby'
-import _get from 'lodash/get'
-import AlertTriangle from 'react-feather/dist/icons/alert-triangle'
+import { StaticQuery, graphql } from 'gatsby'
+import BackgroundVideo from '../components/BackgroundVideo'
+// import _get from 'lodash/get'
+// import AlertTriangle from 'react-feather/dist/icons/alert-triangle'
 
 import Layout from '../components/Layout'
 
-export default ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query NotFoundPageQuery {
-        globalSettings: settingsYaml {
-          siteTitle
+export default ({ children }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        {
+          settingsYaml {
+            siteTitle
+            errorVideoPoster
+            errorVideo
+          }
         }
-      }
-    `}
-    render={data => (
-      <Layout>
-        <Helmet>
-          <title>404 – Page Not Found</title>
-        </Helmet>
-        <section className="section thick">
-          <div className="container skinny taCenter">
-            <p>
-              <AlertTriangle size="5rem" />
-            </p>
-            <h1>404 - Page Not Found</h1>
-            <p>
-              We can't find the page you are looking for!
-              <br />
-              Head back to{' '}
-              <Link to="/">{_get(data, 'globalSettings.siteTitle')}</Link>
-            </p>
-          </div>
-        </section>
-      </Layout>
-    )}
-  />
-)
+      `}
+      render={data => {
+        const { siteTitle, errorVideo, errorVideoPoster } =
+          data.settingsYaml || {}
+
+        return (
+          <Layout>
+            <Helmet>
+              <title>{siteTitle} 404 – Page Not Found</title>
+            </Helmet>
+            <section className="BackgroundVideo-section section error">
+              <BackgroundVideo poster={errorVideoPoster}>
+                {errorVideo && (
+                  <source src={errorVideo} type="video/mp4" />
+                )}
+              </BackgroundVideo>
+            </section>
+          </Layout>
+        )
+      }}
+    />
+  )
+}
